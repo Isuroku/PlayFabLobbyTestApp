21aae1f1c26640f99d26bdca31a9905912cb9f3a
Andrey Ekimov <isurokubox@gmail.com>
2023-02-07 12:38:19 +0100

    start empty project 2

---
+++ Source/PlayFabLobbyTestApp/PlayFabLobbyTestAppGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PlayFabLobbyTestAppGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAYFABLOBBYTESTAPP_API APlayFabLobbyTestAppGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
